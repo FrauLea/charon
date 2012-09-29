ba242fa5f76f5d11e095800ac57b6b0cc0fee6c8
FrauLea <Lea.Gscheidel@gmail.com>
2012-09-30 00:15:41 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charon::Application.config.secret_token = 'b3d524bf5fe6036ff0f2ccc0b266165e4fe7cd3c2ad78c67e48f53006325c0d9b2e389d9ef48729cdf6d5c33149acacb8748b2240452d60fc61eb658602761e7'
